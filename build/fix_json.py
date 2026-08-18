ef7fdadfaa345de04eabd2cc78365f7b48c4e4d3
Michele Pinamonti <michele.pinamonti@gmail.com>
2026-08-18 15:42:50 +0200

    Wood elves!

--- build/fix_json.py
+++ build/fix_json.py
@@ -28,6 +28,42 @@ def pretty_json(data):
     data = re.sub(r'"dc":','"att":', data)
     data = re.sub(r'"special":','"spec":', data)
 
+    data = re.sub(r'"id": "campione", "name": "campione",'
+                  ,'"id": "campione", "name": "campione", "stat_modifiers": {"Attacco": 1},', data)
+    data = re.sub(r'"id": "scudo", "name": "scudo",'
+                  ,'"id": "scudo", "name": "scudo", "add_equipment": "scudo", "stat_modifiers": {"Difesa": 1},', data)
+    data = re.sub(r'"id": "armatura_pesante", "name": "armatura pesante",'
+                  ,'"id": "armatura_pesante", "name": "armatura pesante", "add_equipment": "armatura pesante", "stat_modifiers": {"Difesa": 1},', data)
+    data = re.sub(r'"id": "arma_a_due_mani", "name": "arma a due mani",'
+                  ,'"id": "arma_a_due_mani", "name": "arma a due mani", "add_equipment": "arma a due mani", "stat_modifiers": {"Attacco": 1},', data)
+    data = re.sub(r'"id": "due_armi", "name": "due armi",'
+                  ,'"id": "due_armi", "name": "due armi", "add_equipment": "due armi", "stat_modifiers": {"Attacco": 1},', data)
+    data = re.sub(r'"id": "lancia", "name": "lancia",'
+                  ,'"id": "lancia", "name": "lancia", "add_equipment": "lancia", "add_spec": {"Attacco": "+1 in carica (se a cavallo)"},', data)
+    data = re.sub(r'"id": "lancia_da_cavaliere", "name": "lancia da cavaliere",'
+                  ,'"id": "lancia_da_cavaliere", "name": "lancia da cavaliere", "add_equipment": "lancia da cavaliere", "add_spec": {"Attacco": "+2 in carica (se a cavallo)"},', data)
+    data = re.sub(r'"id": "alabarda", "name": "alabarda",'
+                  ,'"id": "alabarda", "name": "alabarda", "add_equipment": "alabarda", "add_spec": {"Attacco": "+1 se non carica e unità 8+"},', data)
+    data = re.sub(r'"id": "cavallo", "name": "cavallo",'
+                  ,'"id": "cavallo", "name": "cavallo", "add_mount": "cavallo", "stat_modifiers": {"Movimento": 1, "Corpo": 1}, "change_type": {"fanteria": "cavalleria_leggera"},', data)
+    data = re.sub(r'"id": "falco_gigante", "name": "falco gigante",'
+                  ,'"id": "falco_gigante", "name": "falco gigante", "add_mount": "falco gigante", "stat_modifiers": {"Corpo": 1}, "change_type": {"fanteria": "cavalleria_volante"}, "add_spec": {"Movimento": "movimento in volo 4 caselle"},', data)
+    data = re.sub(r'"id": "cavallo_bardato", "name": "cavallo bardato",'
+                  ,'"id": "cavallo_bardato", "name": "cavallo bardato", "add_mount": "cavallo bardato", "stat_modifiers": {"Movimento": 1, "Difesa": 1, "Corpo": 1}, "change_type": {"fanteria": "cavalleria_pesante"},', data)
+    data = re.sub(r'"id": "drago", "name": "drago",'
+                  ,'"id": "drago", "name": "drago", "add_mount": "drago",', data)
+    data = re.sub(r'"id": "pegaso", "name": "pegaso",'
+                  ,'"id": "pegaso", "name": "pegaso", "add_mount": "pegaso",', data)
+    data = re.sub(r'"id": "grifone", "name": "grifone",'
+                  ,'"id": "grifone", "name": "grifone", "add_mount": "grifone",', data)
+    data = re.sub(r'"id": "manticora", "name": "manticora",'
+                  ,'"id": "manticora", "name": "manticora", "add_mount": "manticora",', data)
+    data = re.sub(r'"id": "viverna", "name": "viverna",'
+                  ,'"id": "viverna", "name": "viverna", "add_mount": "viverna",', data)
+    data = re.sub(r'"id": "ippogriffo", "name": "ippogriffo",'
+                  ,'"id": "ippogriffo", "name": "ippogriffo", "add_mount": "ippogriffo",', data)
+    data = re.sub(r'"id": "aquila", "name": "aquila",'
+                  ,'"id": "aquila_gigante", "name": "aquila gigante", "add_mount": "aquila_gigante",', data)
 
     return data
 
